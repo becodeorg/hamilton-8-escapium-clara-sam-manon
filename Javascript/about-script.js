5c4f689ae15f3d3438c666b771d745609d71a4ad
[Clara Mahaux] <[claramahaux@gmail.com]>
2023-06-25 21:33:42 +0200

    single-blog problem with  responsive

--- Javascript/about-script.js
+++ Javascript/about-script.js
@@ -1,23 +1,5 @@
 
 //CAROUSEL TESTIMONIALS
-document.addEventListener(function() {
-  const carousel = document.querySelector("carousel");
-  const left = document.getElementById("arrow-left");
-  const right = document.getElementById("arrow-right");
 
-  left.addEventListener("click", showPreviousSlideTestimonials);
-  right.addEventListener("click", showNextSlideTestimonials);
 
-  function showPreviousSlideTestimonials() {
-    carousel.scrollBy({
-      left: -carousel.offsetWidth,
-      behavior: "smooth"
-    });
-  }
-  function showNextSlideTestimonials() {
-    carousel.scrollBy({
-      left: carousel.offsetWidth,
-      behavior: "smooth"
-    });
-  }
-});
+//ACCORDION
