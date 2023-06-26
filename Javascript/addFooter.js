3c5a8b89816a82cec5048d1ec32cc7766a19af01
Manon <manon.vdm@hotmail.com>
2023-06-26 14:55:42 +0200

    .

--- Javascript/addFooter.js
+++ Javascript/addFooter.js
@@ -1,5 +1,5 @@
 async function addFooter() {
-    const resp = await fetch("/Html/footer.html")
+    const resp = await fetch("Html/footer.html")
     const html = await resp.text();
     document.body.insertAdjacentHTML("afterend", html);
 }
--- Javascript/addTestimonials.js
+++ Javascript/addTestimonials.js
@@ -1,5 +1,5 @@
 async function addTestimonials() {
-    const resp = await fetch("/Html/testimonials.html")
+    const resp = await fetch("Html/testimonials.html")
     const html = await resp.text();
     const articleTestimonials = document.getElementById("homepage_testimonials")
     articleTestimonials.insertAdjacentHTML("afterend", html);
