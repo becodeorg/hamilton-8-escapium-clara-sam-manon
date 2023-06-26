69b62177829e0907648d06cc88e5d6348ad1bbc2
[Clara Mahaux] <[claramahaux@gmail.com]>
2023-06-26 14:36:57 +0200

    fixes on about page

--- Javascript/about-script.js
+++ Javascript/about-script.js
@@ -1,17 +1,4 @@
 
 //CAROUSEL TESTIMONIALS
 
-const slidesContainer = document.getElementsByClassName("test");
-const slide = document.querySelector(".test-slide");
-const prevButton = document.getElementById("arrow-left");
-const nextButton = document.getElementById("arrow-right");
-nextButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft += slideWidth;
-});
-prevButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft -= slideWidth;
-});
-
 //ACCORDION
